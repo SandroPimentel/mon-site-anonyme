cf7b9beb98642b851bd94ec8abb7254f6e94724e
SandroPimentel <pimentel663@icloud.com>
2025-06-11 10:39:53 +0200

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,15 @@
-import './globals.scss';
+import './globals.scss'
+import Link from "next/link";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body>
         <nav>
-          <a href="/">Mon site anonyme</a>
+          <Link href="/" legacyBehavior><a>Mon site anonyme</a></Link>
           <div>
-            <a href="/">Accueil</a>
-            <a href="/admin">Admin</a>
+            <Link href="/" legacyBehavior><a>Accueil</a></Link>
+            <Link href="/admin" legacyBehavior><a>Admin</a></Link>
           </div>
         </nav>
         <main style={{ maxWidth: 800, margin: '2rem auto', padding: '2rem' }}>
